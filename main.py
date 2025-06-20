dc17cf049a9de267798dc03014674455293fd708
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-20 16:11:08 -0500

    update: port

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ def update_user():
     return jsonify(response), code
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
